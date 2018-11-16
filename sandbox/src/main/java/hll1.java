5cf735638368fd5e53d72d26b32112cebba1d1fc
MarrveI <45068274+MarrveI@users.noreply.github.com>
2018-11-16 17:50:08 +0300

    1111

--- sandbox/src/main/java/hll1.java
+++
@@ -1,8 +0,0 @@
-public class hll1 {
-
-public static void main(String[] args) {
-System.out.println("Hello, world");
-}
-
-}
-
---
+++ sandbox/src/main/java/ru/stqa/java_study/sandbox/hll.java
@@ -0,0 +1,11 @@
+package ru.stqa.java_study.sandbox;
+
+public class hll {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, Стасик засрасик");
+    }
+
+    }
+
