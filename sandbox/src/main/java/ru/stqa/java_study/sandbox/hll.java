95c274b4d87d8ab7cfecf4967dec6f013218831d
MarrveI <45068274+MarrveI@users.noreply.github.com>
2018-11-20 16:37:23 +0300

    add some func

--- sandbox/src/main/java/ru/stqa/java_study/sandbox/hll.java
+++ sandbox/src/main/java/ru/stqa/java_study/sandbox/hll.java
@@ -4,8 +4,27 @@ public class hll {
 
     public static void main(String[] args)
     {
-        System.out.println("Hello, ворлд");
+        hello("world");
+        hello("user");
+        hello("hyuser");
+
+        double l = 5;
+        System.out.println( "Площадь со стороной " + l + " = " + area(l));
+
+        double a = 4;
+        double b = 6;
+        System.out.println("площадь треуг со стор " + a + " и " + b + " = " + area(a, b));
     }
 
+    public static void hello(String somebody) {
+        System.out.println("Hello, " + somebody);
     }
+    public static double area (double len) {
+        return len * len;
+    }
+
+    public static double area (double a, double b) {
+        return a * b;
+    }
+}
 
