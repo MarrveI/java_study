e0c08bdc643160abbeb13fcca32bfde2febbd7cc
MarrveI <45068274+MarrveI@users.noreply.github.com>
2018-11-20 18:31:15 +0300

    edit

--- sandbox/src/main/java/ru/stqa/java_study/sandbox/hll.java
+++ sandbox/src/main/java/ru/stqa/java_study/sandbox/hll.java
@@ -9,21 +9,16 @@ public class hll {
         hello("hyuser");
 
         Square s = new Square(5);
-        System.out.println( "Площадь со стороной " + s.l + " = " + area(s));
+        System.out.println( "Площадь со стороной " + s.l + " = " + s.area());
 
         Rectangle r = new Rectangle(4, 6);
-        System.out.println("площадь треуг со стор " + r.a + " и " + r.b + " = " + area(r));
+        System.out.println("площадь треуг со стор " + r.a + " и " + r.b + " = " + r.area());
     }
 
     public static void hello(String somebody) {
         System.out.println("Hello, " + somebody);
     }
-    public static double area (Square s) {
-        return s.l * s.l;
-    }
 
-    public static double area (Rectangle r) {
-        return r.a * r.b;
-    }
+
 }
 
